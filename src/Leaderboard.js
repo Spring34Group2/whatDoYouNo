281af734504ddc195faf16712cc0ad56c8021c3b
NicoleBrady <nicolebrady.dev@gmail.com>
2021-06-25 21:45:45 -0400

    created components, general structure

---
+++ src/Leaderboard.js
@@ -0,0 +1,15 @@
+const Leaderboard = () => {
+    return (
+        <section className = "leaderboard">
+            <h2>Leaderboard</h2>
+            <ul>
+                <li>
+                    <p>Name</p>
+                    <p>Score</p>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+export default Leaderboard;
