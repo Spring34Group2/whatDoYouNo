1cfbbe1b688dd999f7441b62e47e2ee322b4d8d1
erich <ericdjrichardson@gmail.com>
2021-06-27 16:47:59 -0400

    changed state value from string to boolean

--- src/Game.js
+++ src/Game.js
@@ -2,35 +2,52 @@
 import { useState, useEffect } from 'react';
 
 const Game = ({ wordOne, wordTwo, definition }) => {
-  const [answer, setAnswer] = useState('');
+  const [answer, setAnswer] = useState(false);
+  const [score, setScore] = useState(0);
 
-  useEffect(() => {}, [setAnswer]);
+  // useEffect(() => {}, [setAnswer]);
 
   function handleClick(e) {
     wordTwo === e.target.innerText
-      ? setAnswer("true")      
-      : setAnswer("false")
-      console.log(answer);
+      ? setAnswer(true)      
+      : setAnswer(false)
   }
+
+  function increment() {
+    // if (answer === "true")
+    { setScore(score +1) }
+  }
+  // console.log(score)
 
   return (
     <section className="game">
       <div className="counter">
-        <p>Score: 0</p>
+        <p>Score: {score}</p>
       </div>
       <h3>Definition</h3>
       <p>{definition}</p>
       {/* wordOne comes from the array list */}
       <button
-        onClick={handleClick}
+        onClick=
+        {handleClick}
       >
         {wordOne}
       </button>
       {/* wordTwo comes from the data returned */}
-      <button onClick={handleClick}>{wordTwo}</button>
-      <p>{answer}</p>
+      <button onClick={(e) => {
+        handleClick(e);
+        increment();}
+        // setScore( score + 1)
+        }>{wordTwo}</button>
+      {answer ? <p>yes</p> : <p>noooooooooooo</p>}
     </section>
   );
 };
 
 export default Game;
+
+// counter pseudo 
+  // set useState as integer with value of 0
+  // create function to update state (by + 1)
+  // + 1 if value = true? 
+  // display message(or console log) if successful 
