63b2cc4b40838d65a9edff9be8c5ffaa5fb3b2f8
erich <ericdjrichardson@gmail.com>
2021-07-01 20:45:40 -0400

    Working progress bar

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import arrayList from './arrayList';
 import Form from './Form';
 import Leaderboard from './Leaderboard';
+import Progress from './Progress';
 
 const Game = () => {
 
@@ -109,12 +110,37 @@ const Game = () => {
   }
   console.log(showDefinition);
   console.log(showNextQuestion);
+  // console.log(rounds)
   return (
     <>
       <section className="wrapper">
+          { rounds >= 11 ? null : 
         <div className="counter">
           <p>Round: {rounds}</p>
+          {/* this should get moved to Progress.js if it is to be displayed on the screen */}
+
+          <Progress rounds={rounds}/>
         </div>
+          }
+          
+
+          {/* { rounds === 1 ? (
+            <div className="progressBorder">
+              <div className="progressBar"
+              style={{width: 10%}}>
+              </div>
+            </div> 
+            ) 
+            : null
+          } */}
+
+        {/* <div className="progressBorder">
+          <div style={{width: "10%"}}
+            className="progressBar">
+            </div>
+        </div>
+           */}
+        
 
         <div className="contentContainer">
           <div className="scoreContainer">
